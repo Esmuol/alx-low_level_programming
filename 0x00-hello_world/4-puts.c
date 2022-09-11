1ef7934816b9983f26b2f791ad667747a7d71a69
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-11 05:23:52 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * msin - print the string in the input function   
+ * 
+ * Description: using the main function 
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0 
+ */
+int main(void)
+{ 
+            puts("Programming is like building a multilingual puzzle\n");
+	    return (0);
+}
