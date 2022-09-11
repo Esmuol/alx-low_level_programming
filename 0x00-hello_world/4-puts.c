ad6c0bb8020ffac1c0e41d6bd68b3edcbd4f8e38
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-11 06:28:30 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
  * this program prints "Programming is like building a multilingual puzzle
  * Return:0
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
