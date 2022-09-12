5aa8a8d0dd212ba34bac6e80809a3c5654e905a1
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-12 13:30:50 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 
 	if (L > 5)
 	{
-		printf("%s is %d is %d and is greater than 5\n", char str, n, L);
+		printf("%s is %d is %d and is greater than 5\n", str, n, L);
 	}
 	else if (L == 0)
 	{
