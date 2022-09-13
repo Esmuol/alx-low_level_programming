3ee3fbbffe7514462a45c0fa71105cd8ee4035e0
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-12 18:33:20 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is positive, zero or negative
+ * 
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+		int d;
+		for (d =0; d < 100; d++)
+		{
+			putchar ((d / 10) + '0');
+			putchar ((d % 10) + '0');
+
+			if (d != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+
+		return (0);
+}
