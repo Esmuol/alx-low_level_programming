5750dc6df866647b8b5b821a66622bd0aa7e35d4
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-12 17:48:45 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero or negative
+ *
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
