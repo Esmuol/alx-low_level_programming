176586ce8a088b40943a1f3c893b497fd67fa117
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-14 13:21:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN.H 
-#define MAIN.H
+#ifndef MAIN_H 
+#define MAIN_H
 
-int _putchar(charc);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
