84aa56b918987113fadad0f575b8b91198b74162
Esmael Ibrahim <Esmael Ibrahimgit config --global user.email Esmael>
2022-09-21 13:09:42 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates the string pointed to src to 
- * the end of the string pointed to dest 
+ *_strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed to @dest
  *@dest: String that will be appended
- *@src: String to be concatenated upon 
+ *@src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
